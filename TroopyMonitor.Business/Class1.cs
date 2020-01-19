5b7bd1ecd85bc22d05a00494499cfc2384e0f172
DESKTOP\Boss <david@gwynn-jones.com.au>
2020-01-19 12:14:24 +1100

    Add project files.

---
+++ TroopyMonitor.Business/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TroopyMonitor.Business
+{
+    public class Calculator
+    {
+        public static int CalculateAge(DateTime date)
+        {
+            var result = (DateTime.Now - date).Days;
+            return result;
+        }
+    }
+}
---
+++ TroopyMonitor.Test/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using TroopyMonitor.Business;
+
+namespace TroopyMonitor.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void dd()
+        {
+            Trace.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+
+        [Test]
+        public void Get_Age()
+        {
+            // return "You are " + result + " days old.";
+            Trace.WriteLine(">>>> " + Calculator.CalculateAge(DateTime.Parse("1960-5-5")));
+        }
+    }
+}
